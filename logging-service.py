cdd18c50a88c53d8484fc5d5500f16e3dfdd9e11
beheni <anastasiia.beheni@ucu.edu.ua>
2024-02-12 14:08:00 +0100

    seems to be done

--- logging-service.py
+++ logging-service.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import logging
 
-
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -13,7 +12,6 @@ class LogEntity(BaseModel):
     UUID: str 
     message: str
 
-
 @app.get("/log")
 async def list_log():
     return {"messages": ", ".join(list(LOGGED_MESSAGES_MAP.values()))}
@@ -21,7 +19,7 @@ async def list_log():
 @app.post("/log")
 async def log(log_entity: LogEntity):
     if log_entity.UUID in LOGGED_MESSAGES_MAP.keys():
-        logger.warning("UUID already exists in the log map.")
+        logger.warning(f"UUID {log_entity.UUID} already exists in the log map.")
     LOGGED_MESSAGES_MAP[log_entity.UUID] = log_entity.message
     logger.info("Message logged correctly")
     return {"status": "success"}
