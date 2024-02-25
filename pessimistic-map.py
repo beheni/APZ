29a7286cd6a3e4eaa3808c58114a1fea3ea5220f
beheni <anastasiia.beheni@ucu.edu.ua>
2024-02-25 13:34:01 +0200

    finished maps2

--- pessimistic-map.py
+++ pessimistic-map.py
@@ -6,11 +6,13 @@ def increment_key_pessimistic():
     my_map = client.get_map("my-map3").blocking()
     my_map.put_if_absent("key", 0)
     for _ in range(10000):
-        my_map.lock("key")
-        value = my_map.get("key")
-        value += 1
-        my_map.put("key", value)
-        my_map.unlock("key")
+        try:
+            my_map.lock("key")
+            value = my_map.get("key")
+            value += 1
+            my_map.put("key", value)
+        finally:
+            my_map.unlock("key")
     client.shutdown()
 
 if __name__ == "__main__":
