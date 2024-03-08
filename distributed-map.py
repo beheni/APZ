95db9c50ede5bd3d095929a4570d6e1180d61929
beheni <anastasiia.beheni@ucu.edu.ua>
2024-03-08 16:05:35 +0200

    deleted previous lab

--- distributed-map.py
+++
@@ -1,8 +0,0 @@
-import hazelcast
-
-if __name__ == "__main__":
-    client = hazelcast.HazelcastClient()
-    my_map = client.get_map("my-distributed-map").blocking()
-    for i in range(1000):
-        my_map.put(i, i)
-    client.shutdown()
