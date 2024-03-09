86a12e90741a740e22b9d1f8d2f76c3b179b69e4
beheni <anastasiia.beheni@ucu.edu.ua>
2024-03-09 14:20:29 +0200

    added randomising

--- facade-service.py
+++ facade-service.py
@@ -8,15 +8,18 @@ app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-LOGGING_SERVICE_URL = "http://localhost:8001"
+LOGGING_SERVICE_URLS = ["http://localhost:8001", "http://localhost:8002", "http://localhost:8003"]
 MESSAGES_SERVICE_URL = "http://localhost:8002"
+
+def get_random_logging_client():
+    return LOGGING_SERVICE_URLS[random.randint(0, 2)]
 
 class Message(BaseModel):
     text: str
 
 @app.get("/facade_service")
 async def get_messages():
-    logging_response = requests.get(f"{LOGGING_SERVICE_URL}/log")
+    logging_response = requests.get(f"{get_random_logging_client()}/log")
     if logging_response.status_code == 200:
         logger.info("Logging service responded correctly")
     else:
@@ -35,10 +38,11 @@ async def get_messages():
 async def post_messages(msg: Message):
     UUID = str(uuid4())
     payload = {"UUID": UUID, "message": msg.text}
-    response = requests.post(f"{LOGGING_SERVICE_URL}/log", json=payload)
+    response = requests.post(f"{get_random_logging_client()}/log", json=payload)
     
     if response.status_code == 200:
         logger.info(f"Message \"{msg.text}\" logged correctly with UUID {UUID}")
     else:
         logger.critical("Error in POST request the logging service")
     return {"status": "success"}
+
